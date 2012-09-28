a3fabc70d57142a22a5157ab7576103916e95c8d
Gao Chun <chun.gao@intel.com>
2012-09-28 09:15:49 +0800

    Load categories and apps with XHR.

---
+++ js/background.js
@@ -0,0 +1,139 @@
+﻿Updater = {
+  firstDetectTime : 10 * 1000,
+  /*10s after start runtime*/
+  timeInterval : 60 * 60 * 1000,
+  /*1 hour.*/
+  timeoutValue : 5 * 1000,
+  checkUrl : "http://wrt-server.sh.intel.com/service/update2/crx",
+  googleCheckUrl : "http://clients2.google.com/service/update2/crx",
+  showNotification : true,
+  
+  checkUpdate : function (manual) {
+    // Set timer for next detection.
+    if (!manual)
+      setTimeout(Updater.checkUpdate, Updater.timeInterval);
+    
+    chrome.management.getAll(function (results) {
+      var param = "";
+      var paramForGoogle = "";
+      for (var i in results) {
+        if (results[i].isApp) {
+          if (results[i].id == 'licffdjdbhojjgheefcjeddlepijjoee')
+            continue;
+          if (typeof(results[i].updateUrl) != "undefined") {
+            var a = results[i].updateUrl.substr(0, Updater.googleCheckUrl.length);
+            a = "";
+          }
+          if (typeof(results[i].updateUrl) == "undefined" ||
+            results[i].updateUrl.substr(0, Updater.googleCheckUrl.length) != Updater.googleCheckUrl)
+            param += 'x=id%3D' + results[i].id + '%26v%3D' + results[i].version + '%26uc' + '&';
+          else
+            paramForGoogle += 'x=id%3D' + results[i].id + '%26v%3D' + results[i].version + '%26uc' + '&';
+        }
+      };
+      
+      var apps = [];
+      
+      function requsetUpdateManifest(url, params) {
+        var xhr = new XMLHttpRequest();
+        params = params.substr(0, params.length - 1);
+        xhr.open("GET", url + '?' + params, true);
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+              console.log("Response from " + url + " received");
+              var doc = xhr.responseXML;
+              if (doc != null) {
+                var child = doc.documentElement.firstChild;
+                while (child != null) {
+                  if (child.nodeName == 'app') {
+                    var updateCheckNode = child.getElementsByTagName("updatecheck").item(0);
+                    var status = updateCheckNode.getAttribute('status');
+                    if (status == "ok") {
+                      var version = updateCheckNode.getAttribute('version');
+                      if (version == '')
+                        version = '0.0.0.0';
+                      var app = {
+                        'id' : child.getAttribute('appid'),
+                        'version' : updateCheckNode.getAttribute('version'),
+                        'url' : updateCheckNode.getAttribute('codebase')
+                      }
+                      apps.push(app);
+                    }
+                  }
+                  child = child.nextSibling;
+                }
+              }
+            }
+            
+            if (url != Updater.googleCheckUrl) {
+              // In second round, check apps in google update store.
+              requsetUpdateManifest(Updater.googleCheckUrl, paramForGoogle);
+              console.log("Check update in Google update server.")
+            } else {
+              /*if (apps.length > 0) {
+                if (Updater.showNotification) {
+                  // Show notification.
+                  var notification = webkitNotifications.createHTMLNotification('notification.html');
+                  notification.show();
+                  Updater.showNotification = false;
+                  notification.onclose = function () {
+                    Updater.showNotification = true;
+                  };
+                }
+              }
+              // Refresh notification dialog.
+              function notify() {
+                chrome.extension.sendMessage({
+                  updates : apps,
+                  name : 'update_notifiy'
+                });
+              };
+              setTimeout(notify, 500);*/
+              
+              chrome.extension.sendMessage({
+                updates : apps,
+                name : 'update_notifiy'
+              });
+            }
+          }
+        }
+        xhr.onerror = function () {
+          console.log("Check update failed.");
+        }
+        xhr.send();
+        
+        setTimeout(function () {
+          xhr.abort();
+        }, Updater.timeoutValue);
+      }
+      
+      if (param != "") {
+        // Start requesting update info.
+        requsetUpdateManifest(Updater.checkUrl, param);
+        console.log("Check update in lab server.")
+      } else {
+        // Update opend notificaton dialog
+        chrome.extension.sendMessage({
+          updates : [],
+          name : 'update_notifiy'
+        });
+      }
+    });
+  }
+};
+
+// Recheck updates when app is installed or uninstalled.
+chrome.management.onInstalled.addListener(function (info) {
+  Updater.checkUpdate(true);
+});
+chrome.management.onUninstalled.addListener(function (info) {
+  Updater.checkUpdate(true);
+});
+chrome.extension.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.name == "check_update") {
+    Updater.checkUpdate(true);
+  }
+});
+
+setTimeout(Updater.checkUpdate, Updater.firstDetectTime);
--- js/main.js
+++ js/main.js
@@ -1,37 +1,85 @@
 $(function () {
-
-  var loadCategories = function() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "extra/categories.json", true);
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState == 4 && xhr.status == 200) {
-        if (xhr.responseText == null)
-          return;
-        
-        var categories = JSON.parse(xhr.responseText);
-                
-        if (categories.length > 0) {
-          //console.log(categories);
-          for (var i in categories) {
+  var RT24 = {
+    urlCheckAvailable: "http://rt24-labs.sh.intel.com/appinfo?category="
+  };
+  var loadApps = function(category) {
+    // Load categories.
+    $.ajax({
+        url: RT24.urlCheckAvailable+category,
+        dataType: 'json',
+        success: function(apps) {
+          var appGrid = $('ul.thumbnails');
+          appGrid.fadeOut(100);
+          appGrid.children().remove();
+          if (apps.length > 0) {
+            // Render the appTemplate with the "apps" data
+            $("#appTemplate").tmpl(apps).appendTo(appGrid)
+            .mouseenter(function() {
+              var sender = $(this);
+              var timeout = setTimeout(function() {
+                var left = $(this).width() + 6;
+                sender.find(".rt24-thumb").animate({left: left+'px'}, 150);
+              }, 300);
+              
+              var onmouseleave = function(){
+                sender.find(".rt24-thumb").animate({left: 0}, 150);
+                clearTimeout(timeout);
+                $(".row-fluid .rt24-span").unbind('mouseleave', onmouseleave);
+              }
+              
+              $(".row-fluid .rt24-span").bind('mouseleave', onmouseleave);
+            });
             
+            $(".row-fluid .rt24-span").mouseleave(function() {
+            });
           }
-        }
-      }
-    }
-    xhr.onerror = function() {console.log("Get available apps failed.");}
-    xhr.send();
-    
-    setTimeout(function(){ xhr.abort(); }, 5000);
+          appGrid.fadeIn(1000);
+        },
+        error: function() { console.log('Get categories failed'); }
+    });
+  };
+  
+  var loadCategories = function() {
+    // Load categories.
+    $.ajax({
+        url: 'extra/categories.json',
+        dataType: 'json',
+        success: function(categories) {
+          if (categories.length > 0) {
+            //console.log(categories);
+            var list = $('.well > .nav');
+            for (var i in categories) {
+              var item = $('<a>').appendTo($('<li>').appendTo(list))
+                .attr('category', categories[i].data)
+                .attr('href', '#')
+                .html(categories[i].name);
+              
+              /*item.click(function(){
+                if (!$(this).parent().hasClass("active")) {
+                  $(this).parent().parent().children().removeClass("active");
+                  $(this).parent().addClass("active");
+                  onNavItemClick($(this).attr('category'));
+                }
+              });*/
+            }
+            
+            $("ul.nav > li > a").click(function(){
+              if (!$(this).parent().hasClass("active")) {
+                $(this).parent().parent().children().removeClass("active");
+                $(this).parent().addClass("active");
+                loadApps($(this).attr('category'));
+              }
+            });
+          }
+        },
+        error: function() { console.log('Get categories failed'); }
+    });
   }
 
 
   loadCategories();
+  loadApps('new');
 
-  $("ul.nav > li > a").click(function(){
-    $(this).parent().parent().children().removeClass("active");
-    $(this).parent().addClass("active");
-  });
-  
   $(".row-fluid .rt24-span").mouseenter(function() {
     var sender = $(this);
     var timeout = setTimeout(function() {
